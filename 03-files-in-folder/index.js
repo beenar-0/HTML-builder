86dee706d4681b3413746c0c0a4341df9c586e57
beenar-0 <dncastledn@gmail.com>
2022-05-26 18:56:06 +0300

    feat: add files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const fs = require("fs");
+
+const folder = path.resolve(__dirname, 'secret-folder')
+fs.readdir(folder, {withFileTypes: true}, (e, files) => {
+    if (e) throw e
+    files.forEach((item) => {
+        const name = item['name'].match(/[^.]+/)[0]
+        const ext = item['name'].replace(/[^.]+\./, '')
+        const filePath = path.join(__dirname, 'secret-folder', item.name)
+        fs.stat(filePath, (e, stat) => {
+            if (e) throw e
+            const size = stat.size
+            console.log(`${name} - ${ext} - ${size/1000}kb`)
+        })
+    })
+})
+
