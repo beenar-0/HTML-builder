7742f0a4cf414bbbae0ed184f5a48a805f172ef6
beenar-0 <dncastledn@gmail.com>
2022-05-26 21:35:03 +0300

    fix: fix write-file solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,23 @@
 const fs = require('fs');
 const path = require('path')
-const {stdout, stdin} = process
+const {stdout, stdin, exit} = process
 
 
 const writeStream = fs.createWriteStream(path.resolve(__dirname, 'data.txt'))
 
 stdout.write('введите свои данные: \n')
 
-stdin.on('data', (data)=>{
-    writeStream.write(data)
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        stdout.write('exit. Выход');
+        exit();
+    } else {
+        writeStream.write(data);
+    }
+})
+
+process.on('SIGINT', () => {
+    stdout.write('ctl+c. Выход')
+    exit()
 })
 
